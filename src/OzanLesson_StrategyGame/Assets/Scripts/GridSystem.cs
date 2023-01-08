0c79f1b45b21fbd1240fb6223c7161c793a83715
Ozprg <ozanalkankaya@gmail.com>
2023-01-08 16:11:15 +0300

    Grid Sistemi Eklendi

---
+++ src/OzanLesson_StrategyGame/Assets/Scripts/GridSystem.cs
@@ -0,0 +1,22 @@
+
+using UnityEngine;
+
+public class GridSystem : MonoBehaviour
+{
+    
+    
+    void Start()
+    {
+        Grid newGrid = new Grid(100,100);
+       
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+   
+
+}
